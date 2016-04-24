1965c8432679777bea06060a01ac4bdfec31aff8
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-04-25 00:56:45 +0200

    Initial implementation of chrome extension to re-order tabs

---
+++ retab.js
@@ -0,0 +1,23 @@
+function move (where) {
+  chrome.tabs.query({active: true}, function (tabs) {
+    if (!tabs || tabs.length < 1) {
+      return;
+    }
+    var tab = tabs[0];
+    var index = where === 'left' ? tab.index - 1 : tab.index + 1;
+    chrome.tabs.move(tab.id, {index: index});
+  });
+}
+
+chrome.commands.onCommand.addListener(function (command) {
+  switch (command) {
+    case 'move-tab-left':
+      move('left');
+      break;
+    case 'move-tab-right':
+      move('right');
+      break;
+    default:
+      break;
+  }
+});
